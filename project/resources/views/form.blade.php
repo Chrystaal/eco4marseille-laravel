8c1c4b884a8f71d7911d9cd68a8697de2fa401c9
Magda HENNEBO <magdahennebo@MacBook-Pro.local>
2021-11-09 17:44:07 +0100

    dynamic form

--- project/resources/views/form.blade.php
+++ project/resources/views/form.blade.php
@@ -8,33 +8,42 @@
         <input type="text" id="productname" name="productname">
         <label for="producttype"> Type de produit :</label>
         <input type="text" id="producttype" name="producttype">
+    
+    <div>
+        <form action={{ route("store_sub_assembly") }} method=POST>
+        <label for="subassembly"> Quel est le sous-ensemble concerné ? </label>
+        <input type="text" id="subassembly" name="name">
+    </form>
 
     <div>
         <h3> Etape 1 : Matières Premières </h3>
 
-        <label for="weight"> Quelle est sa masse (kg)? :</label>
-        <input type="text" id="weight" name="weight">
-        <label for="material"> Quelle est la matière utilisée ? :</label>
-        <input type="text" id="material" name="material">
-        <label for="origin"> D'où provient-elle ? :</label>
-        <input type="text" id="origin" name="origin">
-        <label for="transformation"> Où est-elle transformée ? :</label>
-        <input type="text" id="transformation" name="transformation">
-        <label for="transportation">Comment est-elle transportée vers le lieu de transformation ? :</label>
-        <input type="text" id="transportation" name="transportation">
+        <form action={{ route("store_raw_material") }} method=POST>
+            <label for="weight"> Quelle est sa masse (kg)? :</label>
+            <input type="text" id="weight" name="weight">
+            <label for="material"> Quelle est la matière utilisée ? :</label>
+            <input type="text" id="material" name="material">
+            <label for="origin"> D'où provient-elle ? :</label>
+            <input type="text" id="origin" name="origin">
+            <label for="transformation"> Où est-elle transformée ? :</label>
+            <input type="text" id="transformation" name="transformation_location">
+            <label for="transportation">Comment est-elle transportée vers le lieu de transformation ? :</label>
+            <input type="text" id="transportation" name="transportation_means_raw">
+        </form>
 
     </div>
     <div>
         <h3> Etape 2 : Fabrication et Assemblage </h3>
-
-        <label for="transportation"> Comment est-il transporté vers le lieu d'assemblage ? :</label>
-        <input type="text" id="transportation" name="transportation">
-        <label for="energy"> Quel est le mix énergétique des différentes usines ? :</label>
-        <input type="text" id="energy" name="energy">
-        <label for="lossratio"> Quel est le taux de pertes résiduelles de la fabrication ? :</label>
-        <input type="text" id="lossratio" name="lossratio">
-        <label for="location"> Où se trouve le lieu d'assemblage ? :</label>
-        <input type="text" id="location" name="location">
+        <form action={{ route("store_assembly") }} method=POST>
+            <label for="transportation"> Comment est-il transporté vers le lieu d'assemblage ? :</label>
+            <input type="text" id="transportation" name="transportation_means_assembly">
+            <label for="energy"> Quel est le mix énergétique des différentes usines ? :</label>
+            <input type="text" id="energy" name="energy">
+            <label for="lossratio"> Quel est le taux de pertes résiduelles de la fabrication ? :</label>
+            <input type="text" id="lossratio" name="loss_rate">
+            <label for="location"> Où se trouve le lieu d'assemblage ? :</label>
+            <input type="text" id="location" name="assembly_location">
+        </form>
     </div>
 
 
--- project/resources/views/formpart2.blade.php
+++ project/resources/views/formpart2.blade.php
@@ -3,47 +3,52 @@
 @section('content')
 <div>
         <h3> Etape 3 : Distribution </h3>
-
+        <form action={{ route("store_distribution") }} method=POST>
         <label for="transportationdis"> Par quel moyen de transport les meubles sont-ils distribués ? :</label>
-        <input type="text" id="transportationdis" name="transportationdis">
+        <input type="text" id="transportationdis" name="transportation_means_dis">
         <label for="packaging"> Comment sont faits vos emballages ? (livraison à plat, carton, zéro plastique et suremballage, etc...) :</label>
-        <input type="text" id="packaging" name="packaging">
+        <input type="text" id="packaging" name="conditioning">
+    </form>
 
     </div>
     <div>
         <h3> Etape 4 : Utilisation </h3>
-
+        <form action={{ route("store_product_use") }} method=POST>
         <label for="glue"> Quelles sont les colles utilisées ? :</label>
         <input type="text" id="glue" name="glue">
         <label for="paint"> Quelles sont les peintures utilisées ? :</label>
         <input type="text" id="paint" name="paint">
+</form>
+<form action={{ route("store_lifespan") }} method=POST>
         <label for="guarantee"> Combien de temps est garanti votre produit ? :</label>
         <input type="text" id="guarantee" name="guarantee">
         <label for="resistance"> Quelle est la résistance mécanique de votre produit ? :</label>
         <input type="text" id="resistance" name="resistance">
+</form>
     </div>
     <div>
         <h3> Etape 5 : Fin de vie </h3>
-
+        <form action={{ route("store_afterlife") }} method=POST>
         <label for="recycling"> Quel est le taux de recyclage du meuble ? :</label>
-        <input type="text" id="recycling" name="recycling">
+        <input type="text" id="recycling" name="recyclable">
         <label for="paint"> Est-il démontable ? :</label>
         <div class="form-check form-switch">
             <label class="form-check-label" for="flexSwitchCheckDefault">oui</label>
-            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" name="movable">
             <label class="form-check-label" for="flexSwitchCheckDefault">non</label>
         </div>
         <label for="guarantee"> Est-il réparable ?? :</label>
         <div class="form-check form-switch">
             <label class="form-check-label" for="flexSwitchCheckDefault">oui</label>
-            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" name="fixable">
             <label class="form-check-label" for="flexSwitchCheckDefault">non</label>
         </div>
         <label for="resistance"> Contribuez-vous à Eco-mobilier ? :</label>
         <div class="form-check form-switch">
             <label class="form-check-label" for="flexSwitchCheckDefault">oui</label>
-            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" name="eco-mobilier">
             <label class="form-check-label" for="flexSwitchCheckDefault">non</label>
+</form><form action={{ route("store_distribution") }} method=POST>
         </div>
 
         <button type="submit" class="btn btn-primary">Valider</button>
